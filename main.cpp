29caa8c7a8fe4ea1909e199f1a5002c6076dcb43
chaosfreak93 <yanluca.lentzen@gmail.com>
2021-11-11 10:20:08 +0100

    Started to add Comments

--- main.cpp
+++ main.cpp
@@ -4,17 +4,24 @@
 
 using namespace std;
 
+//Alle erlaubte Zeichen
 char availableLetters[] = {
         'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
         'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
 };
 
+/*
+ * Wandelt einen String in einen Array um.
+ * Zum Beispiel wird aus
+ * Hallo
+ * H, a, l, l, o
+ */
 char* stringToCharArray(const string& msg) {
-    char* array = new char[0];
+    char* array = new char[0]; //Erstellt einen leeren Array
     for (int i = 0; i < msg.length(); i++) {
-        array[i] = msg[i];
+        array[i] = msg[i]; //Fügt jeden Buchstab des Strings einzeln in denn Array
     }
-    array[msg.length()] = '\0';
+    array[msg.length()] = '\0'; //Fügt ans Ende des Array \0 ein. Behebt ein paar Probleme
     return array;
 }
 
@@ -31,7 +38,7 @@ int getCharArraySize(char *str)
 }
 
 char* encrypt(char *raw) {
-    char* encrypted = new char[0];
+    char* encrypted = new char[0]; //Erstellt einen leeren Array
     for (int i = 0; i < getCharArraySize(raw); i++) {
         char* out = find(begin(availableLetters), end(availableLetters), raw[i]);
         if (out != end(availableLetters)) {
@@ -50,7 +57,7 @@ char* encrypt(char *raw) {
 }
 
 char* decrypt(char *raw) {
-    char* decrypted = new char[0];
+    char* decrypted = new char[0]; //Erstellt einen leeren Array
     for (int i = 0; i < getCharArraySize(raw); i++) {
         char* out = find(begin(availableLetters), end(availableLetters), raw[i]);
         if (out != end(availableLetters)) {
@@ -74,32 +81,32 @@ void menu() {
     cout << "What do you want to do?\n1 = Encrypt\n2 = Decrypt" << endl;
     cin >> mode;
     if (mode == 1) {
-        system("cls");
+        system("cls"); //Leert die Konsole
         cout << "Write what you want to encrypt" << endl;
         cout << "Input: ";
         cin >> input;
         char* output = encrypt(stringToCharArray(input));
         cout << "Encrypted output: " << output << endl;
-        Sleep(2000);
-        system("cls");
-        menu();
+        Sleep(2000); //Wartet 2Sekunde
+        system("cls"); //Leert die Konsole
+        menu(); //Ruft das Menü neu auf
     } else if (mode == 2) {
-        system("cls");
+        system("cls"); //Leert die Konsole
         cout << "Write what you want to decrypt" << endl;
         cout << "Input: ";
         cin >> input;
         char* output = decrypt(stringToCharArray(input));
         cout << "Decrypted output: " << output << endl;
-        Sleep(2000);
-        system("cls");
-        menu();
+        Sleep(2000); //Wartet 2Sekunde
+        system("cls"); //Leert die Konsole
+        menu(); //Ruft das Menü neu auf
     } else {
-        system("cls");
-        menu();
+        system("cls"); //Leert die Konsole
+        menu(); //Ruft das Menü neu auf
     }
 }
 
 int main() {
-    menu();
+    menu(); //Ruft das Menü auf
     return 0;
 }
